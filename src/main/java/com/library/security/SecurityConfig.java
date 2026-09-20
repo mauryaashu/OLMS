6c5954e610ae30fda30eb280a11bc70234666212
ashu <ashu19246@gmail.com>
2026-09-20 21:07:58 +0530

    update CORS configuration to allow requests from production URL

--- src/main/java/com/library/security/SecurityConfig.java
+++ src/main/java/com/library/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173","https://olms.vercel.app"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
